10f3c44db65fa83cb73dbad24518fea8a7f21c1f
liketheflower <jim.morris.shen@gmail.com>
2018-04-01 14:58:27 -0400

    add more code

---
+++ basic_practise3/simple_example.py
@@ -0,0 +1,9 @@
+# this code is collected from the internet below:
+#https://www.analyticsvidhya.com/blog/2018/02/pytorch-tutorial/
+
+
+import torch
+
+# define a tensor
+a = torch.FloatTensor([2])
+print(a)
---
+++ comparison_different_framework/numpy_version.py
@@ -0,0 +1,13 @@
+# this code is used to buid a graph based on the numpy
+# jimmy shen
+# April 1, 2018
+
+
+import numpy as np
+np.random.seed(0)
+
+N, D =3, 4
+
+x = np.random.randn(N, D)
+y = np.random.randn(N, D)
+z = np.random.randn(N, D)
